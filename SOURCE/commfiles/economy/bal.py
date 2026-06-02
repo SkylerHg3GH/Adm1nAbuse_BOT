02bd9b57af45904386565d194ab3b966e7a4c1e6
SkylerHg3GH <consoqu.prense.atioc@proton.me>
2026-06-02 16:37:04 +0700

    Commit the thing

--- SOURCE/commfiles/economy/bal.py
+++
@@ -1,6 +0,0 @@
-from bottype import Context
-import botlib
-
-def main(ctx: Context):
-    srch = ctx.args[0] if len(ctx.args) > 0 else ctx.user
-    botlib.followup(ctx.user, f"{srch} has {botlib.balances[srch]} balance.")
--- SOURCE/commfiles/mistake.py
+++
@@ -1,7 +0,0 @@
-from bottype import Context
-import botlib
-
-def main(ctx: Context):
-    cmdname = ctx.rargs[0]
-    botlib.followup(ctx.user, "Oops! The command you used is a command that seems to exist but doesn't! See !cmds for real commands.")
-    
